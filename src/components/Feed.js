b1aad4fd58a3b2b2b87ad0730412e09d98a15c8b
gregtanym <gregtanym@gmail.com>
2022-07-03 03:30:20 +0800

    editing feed component

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -5,14 +5,15 @@ import { useGlobalContext } from '../context'
 const Feed = () => {
     const {videos, setVideos} = useGlobalContext()
     return (
-        <div>
+        <div className='feed-container'>
             {videos.map((video) => {
                 return(
                     <div key={video.id}>
                         <div>{video.title}</div>
-                        {/* <div className="embed-responsive embed-responsive-16by9">
-                            <iframe className="embed-responsive-item" src={video.video} allowFullScreen></iframe>
-                        </div> */}
+                        {/* in this case, getting the video source does not include the fact that it is from my backend link, so i have to manually add that myself in the video source */}
+                        <div className="embed-responsive embed-responsive-16by9">
+                            <iframe className="embed-responsive-item" src={`http://127.0.0.1:8000${video.video}`} allowFullScreen></iframe>
+                        </div>
                     </div>
                 )
             })}
